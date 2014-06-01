b7d165c8e1b474749cdecd5b25fffb3793ed0052
Kristian Gampong <kgampong1@gmail.com>
2014-05-31 20:47:18 -0700

    Added external java folder

---
+++ java/classifier/MyClass.java
@@ -0,0 +1,18 @@
+package classifier;
+import java.util.ArrayList;
+
+public class MyClass {
+  public static int addNumbers(int a, int b) {
+    return a + b;
+  }
+
+  public static ArrayList<String> returnList() {
+    ArrayList<String> list = new ArrayList<String>();
+    list.add("A");
+    return list;
+  }
+
+  public static String returnString() {
+    return "Hello World!";
+  }
+}
